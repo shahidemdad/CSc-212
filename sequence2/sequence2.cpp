4d0b210ba68c4e5d1a5449802abea6da5c55cec4
shahidemdad <shahidbinemdad@gmail.com>
2020-10-11 14:51:58 -0400

    message

--- sequence2/sequence2.cpp
+++ sequence2/sequence2.cpp
@@ -1,179 +1,204 @@
-/*
-Editor: MdShahid Bin Emdad
-File: sequence2.cpp
-Professor: George Wolberg
-*/
-
-// including libraries
-#include "sequence2.h"
-#include <algorithm>
 #include <assert.h>
+#include "sequence2.h"
+#include<algorithm> // for copy operator
+namespace main_savitch_4
+{
 
-namespace main_savitch_4 {
-    
-    const sequence::size_type sequence::DEFAULT_CAPACITY;
-//  sequence::DEFAULT_CAPACITY is the initial capacity of a sequence that is
+  const sequence::size_type sequence::DEFAULT_CAPACITY;
+//     sequence::DEFAULT_CAPACITY is the initial capacity of a sequence that is
 //     created by the default constructor.
+//
+//
+// CONSTRUCTOR for the sequence class:
 
-    // CONSTRUCTOR and DESTRUCTOR
-    
-    sequence::sequence(sequence::size_type initial_capacity) {
-        m_array_ptr = new sequence::value_type[initial_capacity];
-        m_capacity = initial_capacity;
-        m_currentIdx = 0;
-        m_used = 0;
+   sequence::sequence(size_type initial_capacity)
+   {
+//     Postcondition: The sequence has been initialized as an empty sequence.
+//     The insert/attach functions will work efficiently (without allocating
+//     new memory) until this capacity is reached.
+      capacity=initial_capacity;
+      data=new value_type [capacity];
+      used=0;
+      current_index=0;
+   }
+//
+    sequence::sequence(const sequence& source)
+    {
+//      - Post: we take an already existing source and a new sequence is allocated
+      //if(this!=&source)
+      //{
+        data= new value_type[source.capacity];
+        capacity=source.capacity;
+        used=source.used;
+        current_index=source.current_index;
+        for(size_t i=0;i<used;i++)
+          {
+            data[i]=source.data[i];
+          }
+          //current_index=source.current_index;
+       //}
+	   }
+	sequence::~sequence() // destructor
+	{
+		delete [] data;
+	}
+// MODIFICATION MEMBER FUNCTIONS for the sequence class:
+  void sequence::resize(size_type new_capacity)
+  {
+//     Postcondition: The sequence's current capacity is changed to the 
+//     new_capacity (but not less that the number of items already on the
+//     sequence). The insert/attach functions will work efficiently (without
+//     allocating new memory) until this new capacity is reached.
+     value_type* new_sequence;
+     if(new_capacity==capacity) 
+        return;
+     if(new_capacity<used)
+        new_capacity=used;
+     
+    new_sequence= new value_type[new_capacity];
+     
+     for(size_t i=0;i!=used;i++)
+       {
+        new_sequence[i]=data[i];
+       }
+
+     delete [] data;
+     data=new_sequence;
+     capacity=new_capacity;
+   }
+
+   void sequence::start( )
+   {
+    //if(size()>0) useless
+      current_index=0;
+//     Postcondition: The first item on the sequence becomes the current item
+//     (but if the sequence is empty, then there is no current item).
     }
-
-    sequence::sequence(const sequence& source) {
-        if (this == &source) return ; // check for self assignment
-        m_array_ptr = new sequence::value_type[source.curCapacity()];
-        m_capacity = source.curCapacity();
-        m_currentIdx = source.curIdx();
-        m_used = source.size();
-        std::copy(source.m_array_ptr, source.m_array_ptr+size(), m_array_ptr);
-    }
-
-    sequence::~sequence() {
-        delete [] m_array_ptr;
-    }
-
-    // MODIFICATION MEMBER FUNCTIONS
-    void sequence::start() {
-        m_currentIdx = 0;
-    }
-
-    void sequence::advance( ) {
-        //assert(is_item());
-        //Need to comment out, or sequence_exam won't run.
-        //Added is_item() call to if-statement but still meet precondition requirements
-        if(is_item() && (curIdx() < size())) m_currentIdx++;
-    }
-
-    void sequence::insert(const sequence::value_type& entry) {
-        if (curCapacity() <= size()) resize(size()+1);
-        if(!is_item()) {
-            if (m_used == 0) {
-            m_array_ptr[0] = entry;
-            m_used++;
-            } else {
-            m_currentIdx = 0;
-            for(sequence::size_type i = size(); i > curIdx(); i--) m_array_ptr[i] = m_array_ptr[i-1];
-            m_currentIdx++;
-            m_array_ptr[0] = entry;
-            m_used++;
-            }
+   void sequence::advance( ){
+//     Precondition: is_item returns true.
+    assert(is_item());
+//     Postcondition: If the current item was already the last item on the
+//     sequence, then there is no longer any current item. Otherwise, the new
+//     current item is the item immediately after the original current item.
+    current_index++;
+   }
+   void sequence::insert(const value_type& entry)
+   {
+      if(capacity<=used)
+        resize(used+1);
+      if(!is_item())
+      {
+        current_index=0;
+        for(size_t i=used;i>current_index;i--)
+        {
+          data[i]=data[i-1];
         }
-        else {
-            for (sequence::size_type i = m_used; i > curIdx(); i--) m_array_ptr[i] = m_array_ptr[i-1];
-            m_array_ptr[curIdx()] = entry;
-            m_used++;
+        data[0]=entry;
+      }
+      else{
+        for(size_t i=used;i>current_index;i--)
+        {
+          data[i]=data[i-1];
         }
-    }
+        data[current_index]=entry;
+      }
+      //data[current_index]=entry;
+      used++;
 
-    void sequence::attach(const sequence::value_type& entry) {
-        if (curCapacity() <= size()) resize(size()+1);
-        if (!is_item()) {
-            m_array_ptr[curIdx()] = entry;
-            m_used++;
+//     Postcondition: A new copy of entry has been inserted in the sequence before
+//     the current item. If there was no current item, then the new entry has 
+//     been inserted at the front of the sequence. In either case, the newly
+//     inserted item is now the current item of the sequence.
+   }
+   void sequence::attach(const value_type& entry)
+   {
+      if(capacity<=used)
+        resize(used+1);
+      if(!is_item())
+      {
+       // current_index=used-1;
+       data[current_index]=entry;
+        //used++;
+      }
+      else
+      {
+        //current_index++;
+        for(size_t i=used;i>current_index;i--)
+        {
+          data[i]=data[i-1];
         }
-        else {
-            for(sequence::size_type i = m_used; i > curIdx(); i--) m_array_ptr[i] = m_array_ptr[i-1];
-            m_currentIdx++;
-            m_array_ptr[curIdx()] = entry;
-            m_used++;
-        }
-    }
+        //++current_index;
+       data[++current_index]=entry;
+      }
+      
+      used++;
+//     Postcondition: A new copy of entry has been inserted in the sequence after
+//     the current item. If there was no current item, then the new enrty has 
+//     been attached to the end of the sequence. In either case, the newly
+//     inserted item is now the current item of the sequence.
+   }
+   void sequence::remove_current( )
+   {
+//     Precondition: is_item returns true.
+       assert(is_item());
 
-    void sequence::remove_current( ) {
-        assert(is_item());
-        for(sequence::size_type i = curIdx(); i < size(); i++) m_array_ptr[i] = m_array_ptr[i+1];
-        m_used--;
+    //     Postcondition: The current item has been removed from the sequence, and the
+    //     item after this (if there is one) is now the new current item.
+       for(size_t i=current_index;i<used;i++)
+       {
+       	data[i]=data[i+1];//shift everything
+       }
+       used--;
     }
-
-    void sequence::resize(size_type new_capacity) {
-        sequence::value_type *newArray_ptr;
-        if(curCapacity() == new_capacity) return ;  // nothing to resize
-        else if(curCapacity() > new_capacity) {
-            //assert(size() < new_capacity);
-            /*comment out assertion cause exam won't run otherwise
-            need to test this condition however for when m_capacity is greater
-            and m_used are greater than new_capacity, since some items would be lost
-            in resizing of the array*/
-            if(size() < new_capacity) {
-                newArray_ptr = new sequence::value_type[new_capacity];
-                std::copy(m_array_ptr, m_array_ptr+size(), newArray_ptr);
-                delete [] m_array_ptr;  // free heap to reassign dynamic array
-                m_array_ptr = newArray_ptr;
-                m_capacity = new_capacity;
-            }
-        }
-        else if(curCapacity() < new_capacity) {
-            newArray_ptr = new sequence::value_type[new_capacity];
-            std::copy(m_array_ptr, m_array_ptr+size(), newArray_ptr);
-            delete [] m_array_ptr;  // free heap to reassign dynamic array
-            m_array_ptr = newArray_ptr;
-            m_capacity = new_capacity;
-        }
+// CONSTANT MEMBER FUNCTIONS for the sequence class:
+   sequence::value_type sequence::current( ) const
+   {
+//     Precondition: is_item( ) returns true.
+   	assert(is_item());
+//     Postcondition: The item returned is the current item on the sequence.
+   	return data[current_index];
+  }
+  void sequence::operator=(const sequence& source)
+  {
+   //value_type *new_data;
+    if(this==&source)
+      return;
+    else
+    {
+       delete [] data;
+      data=new value_type[source.capacity];
+      //data=new_data;
+      capacity=source.capacity;
+    current_index=source.current_index;
+    used=source.used;
+      // copy(source.data,source.data + used,data);
+    for(size_t i=0;i<used;i++)
+    {
+      data[i]=source.data[i];
     }
+  }
 
-    void sequence::operator=(const sequence& source) {
-        sequence::value_type *newSeq_ptr;
-        if(this == &source) return; // check self assignment
-        if(curCapacity() != source.curCapacity()) {
-            m_capacity = source.curCapacity();
-            newSeq_ptr = new sequence::value_type[source.curCapacity()];
-            delete [] m_array_ptr; // free heap to reassign dynamic array
-            m_array_ptr = newSeq_ptr;
-        }
-        m_used = source.size();
-        m_currentIdx = source.curIdx();
-        std::copy(source.m_array_ptr, source.m_array_ptr+size(), m_array_ptr);
+  }
+  void sequence::operator+=(const sequence& addend)
+  {
+    size_type add=used+addend.used;
+    if(capacity<add)
+      resize(addend.used+capacity);
+    for(size_t i=0;i!=addend.used;i++)
+    {
+      data[used+i]=addend.data[i];
     }
-
-  // EXTRA CREDIT MEMBER FUNCTION
-    sequence::value_type sequence::operator[](sequence::size_type idx) const {
-        assert(idx >= 0 && idx < size());
-        return m_array_ptr[idx];
-    }
-
-    void sequence::operator+(const sequence& source) {
-        sequence::value_type *newSeq_ptr = new sequence::value_type[curCapacity()+source.curCapacity()];
-        std::copy(m_array_ptr, m_array_ptr+size(), newSeq_ptr);
-        delete [] m_array_ptr;
-        m_array_ptr = newSeq_ptr;
-        for (int i = 0; i < source.size(); i++) this->attach(source[i]);
-    }
-
-    void sequence::operator+=(const sequence& source) {
-        sequence::value_type *newSeq_ptr = new sequence::value_type[curCapacity()+source.curCapacity()];
-        std::copy(m_array_ptr, m_array_ptr+size(), newSeq_ptr);
-        delete [] m_array_ptr;
-        m_array_ptr = newSeq_ptr;
-        for (int i = 0; i < source.size(); i++) this->attach(source[i]);
-    }
-
-    // CONSTANT MEMBER FUNCTIONS
-
-    sequence::size_type sequence::size( ) const {
-        return m_used;
-    }
-
-    sequence::size_type sequence::curIdx( ) const {
-        return m_currentIdx;
-    }
-
-    sequence::size_type sequence::curCapacity( ) const {
-        return m_capacity;
-    }
-
-    bool sequence::is_item( ) const {
-        return (curIdx() < size());
-    }
-
-    sequence::value_type sequence::current( ) const {
-        assert(is_item());
-        return m_array_ptr[curIdx()];
-    }
+    used+=addend.used;
+  }
+  sequence::value_type sequence::operator[](size_type idx) const
+  {
+    assert(is_item());
+    return data[current_index];
+  }
+  sequence operator+(const sequence& s1, const sequence& s2) 
+  {
+    sequence s3=s1;
+    s3+=s2;
+    return s3;
+  }
 }
-
-// END OF THE CODE
--- sequence2/sequence2.h
+++ sequence2/sequence2.h
@@ -115,49 +115,52 @@ CONSTANT MEMBER FUNCTIONS
         Precondition: A positive integer index less than the amount of items in the sequence is entered
         Postcondition: The value at that index is returned
 */
-
 #ifndef MAIN_SAVITCH_SEQUENCE2_H
 #define MAIN_SAVITCH_SEQUENCE2_H
 #include <cstdlib>  // Provides size_t
 
-namespace main_savitch_4 {
-    class sequence {
-      public:
+namespace main_savitch_4
+{
+    class sequence
+    {
+    public:
         // TYPEDEFS and MEMBER CONSTANTS
-            typedef double value_type;
-            typedef size_t size_type; //typedef std::size_t size_type;
-            static const size_type DEFAULT_CAPACITY = 30; //enum {DEFAULT_CAPACITY = 30}; //
-        
+        typedef double value_type;
+        typedef size_t size_type; //typedef std::size_t size_type;
+        static const size_type DEFAULT_CAPACITY = 30; //enum {DEFAULT_CAPACITY = 30}; //
         // CONSTRUCTOR
-            sequence(size_type initial_capacity = DEFAULT_CAPACITY);
-            sequence(const sequence& source);
-            ~sequence( );
-
+        sequence(size_type initial_capacity = DEFAULT_CAPACITY);
+        sequence(const sequence& source);
+        ~sequence( );
         // MODIFICATION MEMBER FUNCTIONS
-            void start( );
-            void advance( );
-            void insert(const value_type& entry);
-            void attach(const value_type& entry);
-            void remove_current( );
-            void resize(size_type new_capacity);
-            void operator=(const sequence& source);
-            //EXTRA CREDIT
-            void operator+(const sequence& source)
-            void operator+=(const sequence& source);
-
+        void start( );
+        void advance( );
+        void insert(const value_type& entry);
+        void attach(const value_type& entry);
+        void remove_current( );
+        void resize(size_type new_capacity);
+        void operator=(const sequence& source);
         // CONSTANT MEMBER FUNCTIONS
-            size_type size( ) const;
-            bool is_item( ) const;
-            // value_type& operator[](size_type idx);
-            value_type operator[](size_type idx) const;
-            value_type current( ) const;
-            size_type curIdx( ) const;
-            size_type curCapacity( ) const;
-      private:
-       // a pointer to a dynamic array, and a size_type
-            value_type *m_array_ptr;
-            size_type m_currentIdx, m_used, m_capacity;
-    };
+        size_type size( ) const;
+        bool is_item( ) const;
+        value_type current( ) const;
+        // value_type& operator[](size_type idx);
+        value_type operator[](size_type idx) const;
+    private:
+// -- Fill in your private member variables here.
+//        -- You'll need a pointer to a dynamic array, and a size_type
+    	value_type *data;
+//        -- variable to keep track of the current length of the
+    	size_type used;
+//        -- sequence, an index to the current item, and 
+    	size_type current_index;
+// -- another size_type variable to keep track of the
+//        -- complete capacity of the dynamic array.
+    	size_type capacity;
+     };
+
+    //non member fucntion:
+    sequence operator+(const sequence& s1, const sequence& s2);
 }
 
 #endif
